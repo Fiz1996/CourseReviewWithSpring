c70220a9b1cb3341eb4e7e9a1365eed638049ad2
Faisal Aleissa <mr.faisal.aleissa@gmail.com>
2021-12-18 14:42:37 +0300

    finished adding paging and sorting

--- src/main/java/com/example/coursereviewapplication/core/DatabaseLoader.java
+++ src/main/java/com/example/coursereviewapplication/core/DatabaseLoader.java
@@ -8,7 +8,9 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Component
 public class DatabaseLoader implements ApplicationRunner {
@@ -22,6 +24,27 @@ public class DatabaseLoader implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         Course course = new Course("Java basics","https://teamtreehouse/com/library/javabasics");
         course.addReview(new Review( 3,"testing"));
-        courses.save(course);
+         courses.save(course);
+        String [] templates= {
+                "Up and Running with %s",
+                "%s basics",
+                "%s for Beginners  "
+        };
+
+        String [] buzzWords = {
+                "Spring data res",
+                "Java for beginners "
+        };
+        List<Course> bunchOfCourses = new ArrayList<>();
+        IntStream.range(0, 100)
+                .forEach(i -> {
+                    String template = templates[i % templates.length];
+                    String buzzword = buzzWords[i % buzzWords.length];
+                    String title = String.format(template, buzzword);
+                    Course c = new Course(title, "http://www.example.com");
+                    c.addReview(new Review(i % 5, String.format("Moar %s please!!!", buzzword)));
+                    bunchOfCourses.add(c);
+                });
+//         courses.save(bunchOfCourses);
     }
 }
--- src/main/java/com/example/coursereviewapplication/review/repository/ReviewRepository.java
+++ src/main/java/com/example/coursereviewapplication/review/repository/ReviewRepository.java
@@ -2,6 +2,7 @@ package com.example.coursereviewapplication.review.repository;
 
 import com.example.coursereviewapplication.review.entity.Review;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ReviewRepository extends CrudRepository<Review,Long> {
+public interface ReviewRepository extends PagingAndSortingRepository<Review,Long> {
 }
